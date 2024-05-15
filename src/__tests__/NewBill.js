b504c01670aeee4d50ed8491ccc5e13c5a568dc6
roy-stephanie <stephanie@shellcore.fr>
2024-05-15 08:29:48 +0200

    NewBill: add test When I am on NewBill Page and UI Control Process is executed

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,9 +3,11 @@
  */
 
 import { fireEvent, screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import { localStorageMock } from '../__mocks__/localStorage.js'
 import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
 import router from '../app/Router.js'
+import mockStore from '../__mocks__/store.js'
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
@@ -61,6 +63,47 @@ describe('Given I am logged in as an employee', () => {
 
       // Programmatic navigation to the NewBill Page
       window.onNavigate(ROUTES_PATH.NewBill)
+    })
+  })
+
+  describe('When I am on NewBill Page and UI Control Process is executed', () => {
+    test('Form submission should trigger submit and file change handlers', async () => {
+      // Define function for navigation to new pages
+      const navigateTo = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      // Simulating a logged in user (Employee)
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+      }))
+
+      document.body.innerHTML = NewBillUI()
+
+      const billsInitialization = new NewBill({
+        document, onNavigate: navigateTo, store: mockStore, localStorage: window.localStorage,
+      })
+
+      // Simulate a file upload process
+      const file = new File(['image'], 'image.png', { type: 'image/png' })
+      const handleChangeFile = jest.fn((e) => billsInitialization.handleChangeFile(e))
+      const newBill = screen.getByTestId('form-new-bill')
+      const fileBill = screen.getByTestId('file')
+
+      fileBill.addEventListener('change', handleChangeFile)
+      userEvent.upload(fileBill, file)
+
+      // Check if file upload function is called
+      expect(fileBill.files[0].name).toBeDefined()
+      expect(handleChangeFile).toBeCalled()
+
+      // Simulate form submission
+      const handleSubmit = jest.fn((e) => billsInitialization.handleSubmit(e))
+      newBill.addEventListener('submit', handleSubmit)
+      fireEvent.submit(newBill)
+
+      // Check if submit function is called
+      expect(handleSubmit).toHaveBeenCalled()
     })
   })
 })
