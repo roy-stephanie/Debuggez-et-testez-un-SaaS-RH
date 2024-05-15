2e964663d8f3014e8af80d44f7aac729df982030
roy-stephanie <stephanie@shellcore.fr>
2024-05-15 08:27:45 +0200

    NewBill: add test When I am on the NewBill Page to save a bill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,46 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from '@testing-library/dom'
+import { localStorageMock } from '../__mocks__/localStorage.js'
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+describe('Given I am logged in as an employee', () => {
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  })
+
+  describe('When I am on the NewBill Page to save a bill', () => {
+    test('Then the bill should be saved correctly', () => {
+      // Define a function to navigate to new page
+      const navigateTo = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+
+      document.body.innerHTML = NewBillUI()
+
+      const billsInitialization = new NewBill({
+        document, onNavigate: navigateTo, store: null, localStorage: window.localStorage,
+      })
+
+      const newBillForm = screen.getByTestId('form-new-bill')
+
+      // Check if the form is rendered correctly
+      expect(newBillForm).toBeTruthy()
+
+      // Test form's submit event
+      const handleSubmit = jest.fn((event) => billsInitialization.handleSubmit(event))
+      newBillForm.addEventListener('submit', handleSubmit)
+      fireEvent.submit(newBillForm)
+
+      // Assert submit event is fired
+      expect(handleSubmit).toHaveBeenCalled()
     })
   })
 })
