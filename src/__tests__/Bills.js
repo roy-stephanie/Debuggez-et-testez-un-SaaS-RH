ba2d89be0bcaaa43e4e56bdd5a9b8690d284c80b
roy-stephanie <stephanie@shellcore.fr>
2024-05-15 08:11:48 +0200

    Bill: Write expect expression

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,37 +2,53 @@
  * @jest-environment jsdom
  */
 
+import '@testing-library/jest-dom'
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import {bills} from "../fixtures/bills.js"
+import {ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
 import router from "../app/Router.js";
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
+// Describes the scenario where the user is an employee
+describe('Given I am logged in as an employee', () => {
+  // Describes the scenario where the user is on Bills Page
+  describe('When I am on Bills Page', () => {
+    // The test where the bill icon in vertical layout should be highlighted
+    test('Then bill icon in vertical layout should be highlighted', async () => {
+      // Setting 'Employee' in localStorage of window
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({type: 'Employee'}))
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
+      // Creating root canvas for navigation
+      const root = document.createElement('div')
+      root.setAttribute('id', 'root')
       document.body.append(root)
+
+      // Initializing router
       router()
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
 
+      // Fetches the window icon
+      const windowIcon = screen.getByTestId('icon-window')
+
+      // Assert the icon has 'active-icon' class
+      expect(windowIcon).toHaveClass('active-icon')
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
+    // The test where the bills should be ordered from earliest to latest
+    test('Then bills should be ordered from earliest to latest', () => {
+      // render Bills UI
+      document.body.innerHTML = BillsUI({data: bills})
+
+      // Fetch all dates
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+
+      // Define antiChrono function for sorting in reverse chronological order
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
+
+      // Assert: The dates should be correctly sorted
       expect(dates).toEqual(datesSorted)
     })
   })
